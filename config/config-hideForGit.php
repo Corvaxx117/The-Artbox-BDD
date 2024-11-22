18d48281a268b44a4d4073e2b9e5274539d642ca
Corvaxx117 <julien.amiel117@gmail.com>
2024-11-22 13:06:33 +0100

    create another config.php to hide informations for git, add config.php to gitignore

---
+++ config/config-hideForGit.php
@@ -0,0 +1,18 @@
+<?php
+
+if (!defined('DB_HOST')) {
+    define("DB_HOST", '****');
+}
+
+if (!defined('DB_NAME')) {
+    define("DB_NAME", '****');
+}
+if (!defined('DB_USER')) {
+    define("DB_USER", '****');
+}
+if (!defined('DB_PASS')) {
+    define("DB_PASS", '****');
+}
+if (!defined('IMAGE_BASE_URL')) {
+    define('IMAGE_BASE_URL', 'chemin/vers/le/dossier/images/');
+}
